91f0886554988bbfa405e2aa9e84471fb2ae9440
tahasohail2 <57339923+tahasohail2@users.noreply.github.com>
2019-11-09 19:09:31 +0500

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+@section('title', 'Welcome 5B')
+
+@section('navbar')
+@parent
+@endsection
+
+@section('content')
+    <div class="jumbotron">
+      <h1>Welcome to Class</h1>      
+      <p>Good luck for your sessional</p>
+      </div>
+   </div>
+@endsection 
